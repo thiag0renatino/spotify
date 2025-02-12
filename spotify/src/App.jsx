9f0630927a1db21bd3e46907a6fce4bbb649e723
Thiago <thiagorenatino@gmail.com>
2025-02-11 22:12:24 -0300

    Home page created

--- spotify/src/App.jsx
+++ spotify/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
+import Main from "./components/Main";
 
 const App = () => {
 
   return (
-    <div>
-        <Header/>
-    </div>
+    <>
+      <Header/>
+      <Main/>
+    </>
   );
 };
 
