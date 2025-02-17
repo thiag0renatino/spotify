1e69ef36674432e16cfe6f4ba90a0983bb9d8c44
Thiago <thiagorenatino@gmail.com>
2025-02-16 23:33:22 -0300

    Bar progress created

--- spotify/src/components/Player.jsx
+++ spotify/src/components/Player.jsx
@@ -14,41 +14,52 @@ const formatTime = (timeInSeconds) => {
     .toString()
     .padStart(2, "0");
   const seconds = Math.floor(timeInSeconds - minutes * 60)
-  .toString()
-  .padStart(2,"0");
+    .toString()
+    .padStart(2, "0");
 
   return `${minutes}:${seconds}`;
 };
 
+const timeInSeconds = (timeString) => {
+  const splitArray = timeString.split(":");
+  const minutes = Number(splitArray[0]);
+  const seconds = Number(splitArray[1]);
+
+  return seconds + minutes * 60;
+
+}
+
 const Player = ({ duration, randomIdFromArtist, audio }) => {
   const audioPlayer = useRef();
+  const progressBar = useRef();
 
   const [isPlaying, setIsPlaying] = useState(false);
 
   const [currentTime, setCurrentTime] = useState(formatTime(0));
 
+  const durationInSeconds = timeInSeconds(duration);
+
+
+
   const playPause = () => {
     isPlaying ? audioPlayer.current.pause() : audioPlayer.current.play();
 
     setIsPlaying(!isPlaying);
-
-   
   };
 
   useEffect(() => {
     const intervalId = setInterval(() => {
       if (isPlaying) {
-        setCurrentTime(formatTime(audioPlayer.current.currentTime)) 
-      };
-      
+        setCurrentTime(formatTime(audioPlayer.current.currentTime));
+
+        progressBar.current.style.setProperty("--_progress", (audioPlayer.current.currentTime / durationInSeconds * 100 + "%"));
+      }
     }, 1000);
-  
+
     return () => {
       clearInterval(intervalId);
-    }
-  }, [isPlaying]) 
-  
-
+    };
+  }, [isPlaying]);
 
   return (
     <div className="player">
@@ -73,7 +84,7 @@ const Player = ({ duration, randomIdFromArtist, audio }) => {
         <p>{currentTime}</p>
 
         <div className="player__bar">
-          <div className="player__bar-progress"></div>
+          <div ref={progressBar}  className="player__bar-progress"></div>
         </div>
 
         <audio ref={audioPlayer} src={audio}></audio>
